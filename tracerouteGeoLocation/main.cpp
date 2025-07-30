9982340a3231a4f80fdfd0f88d97b5abc54be79e
teflonside <atel215@aucklanduni.ac.nz>
2025-07-30 14:22:00 +1200

    Response decoding implemented.

--- tracerouteGeoLocation/main.cpp
+++ tracerouteGeoLocation/main.cpp
@@ -9,6 +9,11 @@
 
 #define PACKET_SIZE 64
 
+#define ICMP_ECHO_REPLY 0
+#define ICMP_DEST_UNREACH 3
+#define ICMP_TTL_EXPIRE 11
+#define ICMP_ECHO_REQUEST 8
+
 typedef struct {
 	unsigned char icmp_type;
 	unsigned char icmp_code;
@@ -75,6 +80,12 @@ int run(int argc, char** argv) {
 	char sendBuf[PACKET_SIZE]; memset(sendBuf, 0, sizeof(sendBuf));
 	char recvBuf[1024]; memset(recvBuf, 0, sizeof(recvBuf));
 	int ttl = 30;
+	int seq_no = 0;
+	if (argc > 2) {
+		int temp = atoi(argv[2]);
+		if (temp != 0)
+			ttl = temp;
+	}
 
 	//Create raw socket.
 	SOCKET rSock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
@@ -83,24 +94,19 @@ int run(int argc, char** argv) {
 		return 1;
 	}
 	
-	BOOL bOptVal = TRUE;
-	//Allow modification of IP header.
-	//if (setsockopt(rSock, IPPROTO_IP, IP_HDRINCL, (char*)&bOptVal, sizeof(BOOL)) == SOCKET_ERROR) {
+	//Set ttl.
 	if (setsockopt(rSock, IPPROTO_IP, IP_TTL, (const char*)&ttl, sizeof(ttl)) == SOCKET_ERROR) {
 		printf("Error setting socket options! Error: %ld\n", WSAGetLastError());
 		return 1;
 	}
 
-	//Construct headers
-	//iphdr* iph = (iphdr*)sendBuf;
-	//icmphdr* icmph = (icmphdr*)(sendBuf + sizeof(iphdr));
-	icmphdr* icmph = (icmphdr*)sendBuf;
+	//Construct header
+	icmphdr* sicmph = (icmphdr*)sendBuf;
 
 	const char* msg = "Transgender pinging you!";
 
-	//memcpy(sendBuf + sizeof (iphdr) + sizeof (icmphdr), msg, strlen(msg) + 1);
 	memcpy(sendBuf + sizeof(icmphdr), msg, strlen(msg) + 1);
-	sockaddr_in dest, source;
+	sockaddr_in dest, from;
 	in_addr srcAddr;
 
 	if (get_external_ip(&srcAddr) == 1)
@@ -110,30 +116,14 @@ int run(int argc, char** argv) {
 	dest.sin_port = htons(33434);
 	inet_pton(AF_INET, argv[1], &dest.sin_addr);
 
-	/*
-	
-	iph->ip_hl = 5;
-	iph->ip_v = 4;
-	iph->ip_tos = 0;
-	iph->ip_len = sizeof (iphdr) + sizeof (icmphdr);
-	iph->ip_id = 1;
-	iph->ip_off = 0;
-	iph->ip_ttl = 30;
-	iph->ip_p = 1;
-	iph->ip_sum = 0;
-	inet_pton(AF_INET, inet_ntoa(srcAddr), &iph->ip_src);
-	iph->ip_dst = dest.sin_addr.s_addr;
-	*/
+	sicmph->icmp_type = ICMP_ECHO_REQUEST;
+	sicmph->icmp_code = 0;
+	sicmph->icmp_sum = 0;
+	sicmph->icmp_id = (USHORT)GetCurrentProcessId();
+	sicmph->icmp_seq = seq_no;
+	sicmph->timestamp = GetTickCount();
 
-	icmph->icmp_type = 8; //ICMP_ECHO_REQUEST
-	icmph->icmp_code = 0;
-	icmph->icmp_sum = 0;
-	icmph->icmp_id = (USHORT)GetCurrentProcessId();
-	icmph->icmp_seq = 0;
-	icmph->timestamp = GetTickCount();
-
-	//iph->ip_sum = checksum((unsigned short*)&iph, sizeof (iphdr));
-	icmph->icmp_sum = checksum((unsigned short*)icmph, sizeof (sendBuf));
+	sicmph->icmp_sum = checksum((unsigned short*)sicmph, sizeof (sendBuf));
 
 	printf("Sending packet to %s.\n", inet_ntoa(dest.sin_addr));
 	int val = sendto(rSock, sendBuf, sizeof (sendBuf), 0, (sockaddr*)&dest, sizeof(dest));
@@ -143,8 +133,8 @@ int run(int argc, char** argv) {
 	}
 	printf("%ld bytes sent.\n", val);
 
-	int fromlen = sizeof(source);
-	if (recvfrom(rSock, recvBuf, 1024, 0, (sockaddr*)&source, &fromlen) == SOCKET_ERROR) {
+	int fromlen = sizeof(from);
+	if (recvfrom(rSock, recvBuf, 1024, 0, (sockaddr*)&from, &fromlen) == SOCKET_ERROR) {
 		printf("Error receiving packets!");
 		if (WSAGetLastError() == WSAEMSGSIZE) {
 			printf("Buffer too small!\n");
@@ -152,15 +142,57 @@ int run(int argc, char** argv) {
 			printf("Error: %ld", WSAGetLastError());
 		return 1;
 	}
-	printf("Recieved response!\n");
+
+	iphdr* riph = (iphdr*)recvBuf;
+	unsigned short rhlen = riph->ip_hl*4;
+	icmphdr* ricmph = (icmphdr*)(recvBuf + rhlen);
+
+	if (ricmph->icmp_seq != seq_no)
+		printf("Bad sequence number!\n");
+
+	switch (ricmph->icmp_type) {
+	case (ICMP_ECHO_REPLY):
+		break;
+	case (ICMP_DEST_UNREACH):
+		printf("Destination unreachable!\n");
+		return 1;
+		break;
+	case (ICMP_TTL_EXPIRE):
+		break;
+	default:
+		printf("Unknown ICMP packet type!\n");
+		return 1;
+	}
+
+	if (ricmph->icmp_id != (USHORT)GetCurrentProcessId());
+		// Code to ignore packet from another pinger program.
+
+	int nHops = int(256 - riph->ip_ttl);
+	if (nHops == 192)
+		// TTL 64, so ping was probably to a host on the LAN.
+		// Call it a single hop.
+		nHops = 1;
+
+	else if (nHops == 128) {
+		//Probably localHost
+		nHops = 0;
+	}
+
+	printf("\n %d bytes from %s, icmp_seq, ", PACKET_SIZE, inet_ntoa(from.sin_addr), ricmph->icmp_seq);
+	if (ricmph->icmp_type == ICMP_TTL_EXPIRE)
+		printf("TTL Expired.\n");
+	else {
+		printf("%d hops", nHops);
+		printf(", time: %ld ms", GetTickCount() - ricmph->timestamp);
+	}
 
 	return 0;
 }
 
 int __cdecl main(int argc, char **argv) {
-	if (argc != 2) {
-		printf("Please provide an IPv4 address in dotted decimal format.\n");
-		//return 1;
+	if (argc < 2) {
+		printf("Usage: %s <host> [ttl]\n", argv[0]);
+		return 1;
 	}
 
 	WSADATA wsaData;
